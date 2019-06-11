6231e2c8821f764216584b4401277f851e5c1e0f
Zilvinas Bucys <zilbuc@gmail.com>
2019-06-11 16:15:00 +0300

    env var 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,8 @@ class IndexPage extends Component {
 
   componentDidMount = () => {
     document.querySelector('.search-again').setAttribute('style', 'display: block');
-    console.log(process.env.AFI_KEY);
-    console.log(process.env.API_KEY);
+    console.log(process.env.GATSBY_AFI_KEY);
+    console.log(process.env.GATSBY_API_KEY);
   }
 
   onSearchFieldChange = (event) => {
